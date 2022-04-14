69b96874bad751a9b1149e7dd2089da8d58cf048
katie-woods <katherine14.woods@gmail.com>
2022-04-14 17:03:05 +1200

    Longer commented sections

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-# python3 script to calculate required fault plane displacement given the earthquake moment
-# magnitude, rectangular fault dimensions, and assumed shear modulus
+""" python3 script to calculate required fault plane displacement given the earthquake moment
+    magnitude, rectangular fault dimensions, and assumed shear modulus """
 
 def calc_displacement_rect(fault_length, fault_width, shear_mod, mom_mag):
-    # fault length and width in kilometres, shear modulus in GPa
+    """ fault length and width in kilometres, shear modulus in GPa
+    """
 
     # calculate rectangle area in metres-squared
     area = fault_length * fault_width * 1E6
 
     # convert shear modulus from GPa to Pa
-    shear_mod = shear_mod * 1E9
+    shear_mod *= 1E9
 
     # calculate displacement in metres
     # seismic moment (Mo) = shear_mod * fault_area * displacement       in N m
